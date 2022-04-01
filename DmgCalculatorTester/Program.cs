f86dddd0b940c7516cf5ec4804337f93b60c057f
Piotr s <vperunv@gmail.com>
2022-04-01 10:43:56 +0200

    Add project files.

---
+++ DmgCalculatorTester/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DmgCalculatorTester
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        public decimal MagicMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FlamingDamage;
+        }
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+        }
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            SwordDamage swordDamage = new SwordDamage();
+            while (true)
+            {
+                Console.Write("0 - ani magiczny, ani płonący; 1 - magiczny; 2 - płonący; " +
+                                "3 - magiczny i płonący; inne wartości - koniec: ");
+                char key = Console.ReadKey().KeyChar;
+                if (key != '0' && key != '1' && key != '2' && key != '3') return;
+                int roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+                swordDamage.Roll = roll;
+                swordDamage.SetMagic(key == '1' || key == '3');
+                swordDamage.SetFlaming(key == '2' || key == '3');
+                Console.WriteLine("\nRzut: " + roll + ", punkty obrażeń: " +
+                                      swordDamage.Damage + "\n");
+            }
+        }
+    }
+}
